33e6ce81df1a5d48402a6341eef250afeb23bc83
ZeynepNurDilek <zeynepnurdilek@hotmail.com>
2014-01-08 20:26:58 +0200

    gorevli - tc sorgulama

--- OnlineOylama/src/main/java/Data.java
+++ OnlineOylama/src/main/java/Data.java
@@ -14,22 +14,42 @@ import javax.faces.bean.ManagedBean;
 @ManagedBean( name = "data" )
 public class Data {
     private int tc;
-    private int adres;
+    private int bilg_adres;
+    private int oy_adres;
     private int gorevli_id;
-    private String sifre;
+    private int sifre;
     private int ekransecimi;
     
     
-    public String adresal(){
-    if(ekransecimi == 1){
-       adres  = 123;
-       return "oyVer";
+    public String tcSorgula(){
+       
+        if(tc==123)
+            oy_adres = 12344566 ;
+        
+        return "gorevli";        
     }
-    else if(ekransecimi == 2){ 
-       adres = 234;
-       return "gorevli";
+    
+    
+    
+    
+    public String yonlendir(){
+    
+     //dbden gorevli_idnin sifresini çek    
+     
+    if(sifre == 123){   //buraya databaseden çekilecek değer gelecek
+        
+        if(ekransecimi == 1){
+           return "oyVer";
+        }
+        else if(ekransecimi == 2){ 
+           return "gorevli";
+        }
+        else return "index";
     }
-    else return "index";
+    
+    else 
+        return "index";
+    
     }
     
     
@@ -49,20 +69,6 @@ public class Data {
     }
 
     /**
-     * @return the adres
-     */
-    public int getAdres() {
-        return adres;
-    }
-
-    /**
-     * @param adres the adres to set
-     */
-    public void setAdres(int adres) {
-        this.adres = adres;
-    }
-
-    /**
      * @return the gorevli_id
      */
     public int getGorevli_id() {
@@ -79,14 +85,14 @@ public class Data {
     /**
      * @return the sifre
      */
-    public String getSifre() {
+    public int getSifre() {
         return sifre;
     }
 
     /**
      * @param sifre the sifre to set
      */
-    public void setSifre(String sifre) {
+    public void setSifre(int sifre) {
         this.sifre = sifre;
     }
 
@@ -102,6 +108,34 @@ public class Data {
      */
     public void setEkransecimi(int ekransecimi) {
         this.ekransecimi = ekransecimi;
+    }
+
+    /**
+     * @return the bilg_adres
+     */
+    public int getBilg_adres() {
+        return bilg_adres;
+    }
+
+    /**
+     * @param bilg_adres the bilg_adres to set
+     */
+    public void setBilg_adres(int bilg_adres) {
+        this.bilg_adres = bilg_adres;
+    }
+
+    /**
+     * @return the oy_adres
+     */
+    public int getOy_adres() {
+        return oy_adres;
+    }
+
+    /**
+     * @param oy_adres the oy_adres to set
+     */
+    public void setOy_adres(int oy_adres) {
+        this.oy_adres = oy_adres;
     }
     
     
