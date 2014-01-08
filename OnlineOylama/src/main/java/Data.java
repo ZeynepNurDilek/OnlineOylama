a194d02e991dbd5d64b7fa200ade5a7575d783c8
ZeynepNurDilek <zeynepnurdilek@hotmail.com>
2014-01-08 15:26:14 +0200

    giriş ekranı görevli/sifre

--- OnlineOylama/src/main/java/Data.java
+++ OnlineOylama/src/main/java/Data.java
@@ -1,3 +1,7 @@
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -7,14 +11,21 @@
  *
  * @author zeynep
  */
+
+@ManagedBean( name = "data" )
 public class Data {
+    @ManagedProperty(value="#{tc}")
     public int tc;
     public int adres;
+    @ManagedProperty(value="#{gorevli_id}")
+    public int gorevli_id;
+    @ManagedProperty(value="#{sifre}")
+    public String sifre;
     
-    public void sorgula (){
+    public int sorgula (){
     
-        adres= 1234;
-    
+        adres = 1234;
+        return adres;
     }
     
     
