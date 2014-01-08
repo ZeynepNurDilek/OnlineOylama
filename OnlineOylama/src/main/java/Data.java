a31713a34d4ece0a5d0b23e443bf1e6c655257fd
ZeynepNurDilek <zeynepnurdilek@hotmail.com>
2014-01-08 13:58:12 +0200

    tc kimlik deneme

---
+++ OnlineOylama/src/main/java/Data.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author zeynep
+ */
+public class Data {
+    public int tc;
+    public int adres;
+    
+    public void sorgula (){
+    
+        adres= 1234;
+    
+    }
+    
+    
+}
